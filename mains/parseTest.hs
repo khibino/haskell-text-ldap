f8df5ac4d86a5d03a00a664f7e11d944df207bd0
Kei Hibino <ex8k.hibino@gmail.com>
2014-06-20 16:32:54 +0900

    Update parseTest.

--- mains/parseTest.hs
+++ mains/parseTest.hs
@@ -1,10 +1,16 @@
 
+import Control.Applicative
 import Text.LDAP.Parser
 import qualified Data.ByteString.Lazy.Char8 as LB
-import Data.Either
+---  import Data.Either
+
+
+matchTest :: LB.ByteString -> String
+matchTest b = case runLdapParser openLdapEntry b of
+  Right r -> show r
+  Left  e -> "Failed: " ++ show e ++ ": " ++ show b
 
 main :: IO ()
 main =  do
-  in' <- LB.getContents
-  let bs = map LB.unlines . openLdapDataBlocks $ LB.lines in'
-  mapM_ print . lefts . map (runLdapParser openLdapEntry) $ bs
+  bs <- map LB.unlines . openLdapDataBlocks . LB.lines <$> LB.getContents
+  mapM_ (putStrLn . matchTest) $ bs
