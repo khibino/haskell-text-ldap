383708d353a2ead4f80db07c236c095b2c87e008
Kei Hibino <ex8k.hibino@gmail.com>
2014-06-26 12:45:28 +0900

    Add parse mode.

--- mains/parseTest.hs
+++ mains/parseTest.hs
@@ -1,15 +1,22 @@
 
+import System.Environment (getArgs)
 import Control.Applicative
+import Text.LDAP.Data
 import Text.LDAP.Parser
 import qualified Data.ByteString.Lazy.Char8 as LB
 
 
-matchTest :: LB.ByteString -> String
-matchTest b = case runLdapParser (openLdapEntry decodeAttrValue) b of
+matchTest :: Show a => (LdifAttrValue -> LdapParser a) -> LB.ByteString -> String
+matchTest d b = case runLdapParser (openLdapEntry d) b of
   Right r -> show r
   Left  e -> "Failed: " ++ show e ++ ": " ++ show b
 
 main :: IO ()
 main =  do
+  as <- getArgs
+  let test = case as of
+        "raw":_  -> matchTest rawAttrValue
+        _        -> matchTest decodeAttrValue
+
   bs <- map LB.unlines . openLdapDataBlocks . LB.lines <$> LB.getContents
-  mapM_ (putStrLn . matchTest) $ bs
+  mapM_ (putStrLn . test) $ bs
