38ce896cc00e9697350be86334a96628e6cbfb3c
Kei Hibino <ex8k.hibino@gmail.com>
2014-06-27 23:48:27 +0900

    Add generator of AttrType.

--- test/PrintParse.hs
+++ test/PrintParse.hs
@@ -3,31 +3,16 @@ module PrintParse where
 
 import Distribution.TestSuite
   (Test (Test), TestInstance (TestInstance), Result (Pass, Fail), Progress (Finished))
-import Test.QuickCheck (Testable, Gen, choose, elements, vector, Arbitrary (..), quickCheck)
+import Test.QuickCheck
+  (Testable, Gen, Arbitrary (..),
+   choose, oneof, elements, quickCheck)
 
 import Control.Exception (try)
-import System.Random (StdGen, randomR)
-import Text.LDAP.Data (List1)
-import Data.List.NonEmpty ((:|))
+import Control.Applicative ((<$>), (<*>))
+import Data.ByteString.Char8 (ByteString, pack)
+import Text.LDAP.Data (AttrType (..), List1)
+import Data.List.NonEmpty (NonEmpty ((:|)))
 
-digit :: Gen Char
-digit =  choose ('0', '9')
-
-boundInt :: Int -> Int -> Gen Int
-boundInt =  curry choose
-
-list :: Gen a -> Int -> Gen [a]
-list g n = sequence [ g | i <- [1..n] ]
-
-list1 :: Gen a -> Int -> List1 a
-list1 g n = do
-  i <- 1 :| [2..n]
-  return g
-
-digits :: Gen String
-digits =  boundInt 1 7 >>= list digit
-
--- attrByte
 
 simpleInstance :: IO Progress -> String -> Test
 simpleInstance p name = Test this  where
@@ -37,3 +22,42 @@ testSuite :: Testable prop => prop -> String -> Test
 testSuite t = simpleInstance $ do
   e <- try $ quickCheck t
   return . Finished $ either (Fail . show) (const Pass) (e :: Either IOError ())
+
+
+boundInt :: Int -> Int -> Gen Int
+boundInt =  curry choose
+
+list :: Gen a -> Int -> Gen [a]
+list g n = sequence [ g | _i <- [1..n] ]
+
+sequence1 :: Monad m => NonEmpty (m a) -> m (NonEmpty a)
+sequence1 (ma :| ms) = do
+  x   <- ma
+  xs  <- sequence ms
+  return $ x :| xs
+
+list1 :: Gen a -> Int -> Gen (List1 a)
+list1 g n = sequence1 $ do
+  _i <- 1 :| [2..n]
+  return g
+
+digit :: Gen Char
+digit =  choose ('0', '9')
+
+oidpe :: Gen ByteString
+oidpe =  (pack <$>) $ boundInt 1 10 >>= list digit
+
+alpha :: Gen Char
+alpha =  oneof [choose ('A', 'Z'), choose ('a', 'z')]
+
+keychar :: Gen Char
+keychar =  elements $ '-' : ['0'..'9'] ++ ['A'..'Z'] ++ ['a'..'z']
+
+keystr :: Gen ByteString
+keystr =  (pack <$>) $ (:) <$> alpha <*> (boundInt 0 40 >>= list keychar)
+
+attrType :: Gen AttrType
+attrType = oneof
+           [ AttrType <$> keystr
+           , (AttrOid <$>) $ boundInt 1 8 >>= list1 oidpe
+           ]
