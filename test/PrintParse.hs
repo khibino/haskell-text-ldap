ff97063eea332858612ff4f67b4161a75654aa10
Kei Hibino <ex8k.hibino@gmail.com>
2014-07-01 11:26:40 +0900

    Add attribute generator and arbitrary.

--- test/PrintParse.hs
+++ test/PrintParse.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS -fno-warn-orphans #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module PrintParse where
 
@@ -10,7 +12,11 @@ import Test.QuickCheck
 import Control.Exception (try)
 import Control.Applicative ((<$>), (<*>))
 import Data.ByteString.Char8 (ByteString, pack)
-import Text.LDAP.Data (AttrType (..), List1)
+import Text.LDAP.Data (AttrType (..), Attribute, List1)
+import Text.LDAP.Printer (LdapPrinter, runLdapPrinter)
+import qualified Text.LDAP.Printer as Printer
+import Text.LDAP.Parser (LdapParser, runLdapParser)
+import qualified Text.LDAP.Parser as Parser
 import Data.List.NonEmpty (NonEmpty ((:|)))
 
 
@@ -56,8 +62,32 @@ keychar =  elements $ '-' : ['0'..'9'] ++ ['A'..'Z'] ++ ['a'..'z']
 keystr :: Gen ByteString
 keystr =  (pack <$>) $ (:) <$> alpha <*> (boundInt 0 40 >>= list keychar)
 
+bstring' :: Int -> Int -> Gen ByteString
+bstring' n m = (pack <$>) $ boundInt n m >>= list (elements ['\0'..'\255'])
+
+bstring :: Int -> Gen ByteString
+bstring =  bstring' 0
+
+bstring1 :: Int -> Gen ByteString
+bstring1 =  bstring' 1
+
+
+
 attrType :: Gen AttrType
 attrType = oneof
            [ AttrType <$> keystr
            , (AttrOid <$>) $ boundInt 1 8 >>= list1 oidpe
            ]
+
+attribute :: Gen Attribute
+attribute =  (,) <$> attrType <*> bstring 1024
+
+
+prop_iso :: Eq a => LdapPrinter a -> LdapParser a -> a -> Bool
+prop_iso pr ps a = Right a == (runLdapParser ps . runLdapPrinter pr $ a)
+
+instance Arbitrary Attribute where
+  arbitrary = attribute
+
+prop_attribute :: Attribute -> Bool
+prop_attribute =  prop_iso Printer.attribute Parser.attribute
